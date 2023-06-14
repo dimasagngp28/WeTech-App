8f0500eb61a9864b56fb9098f9712c44198f1f10
dimasagngp28 <dimas1900028102@webmail.uad.ac.id>
2023-06-14 20:44:47 +0700

    mengubah tujuan indexhtml

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,8 @@
 import 'regenerator-runtime';
 import '../styles/style.css';
 import '../styles/responsive.css';
+import '../styles/belajar.css';
+import './js/belajar';
+import './js/script';
 
 console.log('Hello Coders!');
