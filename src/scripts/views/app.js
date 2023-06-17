6afacff3c1282f86410b8a0bd703f20a5e409a24
dimasagngp28 <dimas1900028102@webmail.uad.ac.id>
2023-06-17 21:55:04 +0700

    memasukkan logic javascript utk uiux.js

--- src/scripts/views/app.js
+++ src/scripts/views/app.js
@@ -25,6 +25,30 @@ class App {
     const url = UrlParser.parseActiveUrlWithCombiner();
     const page = routes[url];
     this._content.innerHTML = await page.render();
+    const sidebar = this._content.querySelector('.sidebar');
+    if (sidebar) {
+      const sidebarClose = this._content.querySelector('#sidebar-close');
+      const menu = this._content.querySelector('.menu-content');
+      const menuItems = this._content.querySelectorAll('.submenu-item');
+      const subMenuTitles = this._content.querySelectorAll('.submenu .menu-title');
+      sidebarClose.addEventListener('click', () => sidebar.classList.toggle('close'));
+      menuItems.forEach((item, index) => {
+        item.addEventListener('click', () => {
+          menu.classList.add('submenu-active');
+          item.classList.add('show-submenu');
+          menuItems.forEach((item2, index2) => {
+            if (index !== index2) {
+              item2.classList.remove('show-submenu');
+            }
+          });
+        });
+      });
+      subMenuTitles.forEach((title) => {
+        title.addEventListener('click', () => {
+          menu.classList.remove('submenu-active');
+        });
+      });
+    }
   }
 }
 
