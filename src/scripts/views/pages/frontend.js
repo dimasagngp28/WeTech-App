f4e959da9a2909edc36e3250549a2731e62f32ba
M. Rosyadi Aulawi <dadi15122001@gmail.com>
2023-06-19 12:36:07 +0800

    menambah materi

--- src/scripts/views/pages/frontend.js
+++ src/scripts/views/pages/frontend.js
@@ -76,6 +76,28 @@ const FrontEnd = {
       Sedangkan UX (User Experience) adalah sebuah proses untuk mempermudah pengguna dalam mencapai tujuannya saat menggunakan semua aspek dari sebuah produk, baik itu produk digital ataupun produk non-digital, dan diselarasikan dengan tujuan bisnis dari perusahaan produsen produk tersebut.
       </p>
     </div>
+
+    <div id="perbedaanUIUX">
+    <h2>Apa itu Javascript</h2>
+    <p>
+    JavaScript adalah bahasa pemrograman yang digunakan untuk mengembangkan fitur interaktif pada halaman web. Dengan JavaScript, Anda dapat mengontrol elemen HTML, memanipulasi tampilan, merespons tindakan pengguna, dan berkomunikasi dengan server. Hal ini memungkinkan pembuatan halaman web yang dinamis dan interaktif, meningkatkan pengalaman pengguna dan fungsionalitas situs.s
+    </p>
+
+    <p>
+    Selain itu, JavaScript juga memiliki fitur yang memungkinkan penggunaan asynchronous programming, yaitu kemampuan untuk menjalankan tugas secara bersamaan tanpa harus menunggu tugas sebelumnya selesai. Hal ini memungkinkan pengembangan aplikasi yang responsif dan efisien, terutama saat melakukan operasi jaringan seperti pengambilan data dari server atau pemrosesan data yang membutuhkan waktu lama.
+    </p>
+  </div>
+
+  <div id="perbedaanUIUX">
+  <h2>Sejarah Javascript</h2>
+  <p>
+  JavaScript dibuat pada tahun 1995 oleh Brendan Eich ketika ia bekerja di Netscape Communications Corporation. Awalnya dikenal sebagai "Mocha" dan kemudian diubah namanya menjadi "LiveScript" sebelum akhirnya menjadi JavaScript. JavaScript dirancang sebagai bahasa scripting untuk memberikan interaktivitas pada halaman web. Dalam beberapa tahun, JavaScript mendapatkan popularitas yang besar dan menjadi bahasa pemrograman yang paling penting dalam pengembangan web. Standarisasi JavaScript dilakukan oleh European Computer Manufacturers Association (ECMA), yang menghasilkan spesifikasi standar ECMAScript. ECMAScript terus mengalami perkembangan dan pembaruan, dengan versi terbaru seperti ECMAScript 6 (ES6) memperkenalkan fitur-fitur baru yang memperluas kemampuan bahasa JavaScript. Saat ini, JavaScript digunakan secara luas dalam pengembangan web modern, dari pembuatan efek interaktif sederhana hingga pengembangan aplikasi web yang kompleks dan responsif.
+  </p>
+
+  <p>
+  JavaScript telah menjadi bahasa pemrograman yang tidak terpisahkan dari pengembangan web modern. Dalam sejarahnya, JavaScript telah mengalami evolusi yang signifikan dan menjadi landasan utama dalam menciptakan pengalaman interaktif di web. Dalam perkembangan web saat ini, JavaScript tidak hanya digunakan di sisi klien (client-side), tetapi juga di sisi server (server-side) melalui platform seperti Node.js. Dengan dukungan dari berbagai library dan framework seperti React, Angular, dan Vue.js, JavaScript memungkinkan pengembang untuk membangun aplikasi web yang kompleks dengan kode yang lebih terstruktur dan terkelola. Dengan terus berkembangnya teknologi web, JavaScript terus beradaptasi dan menjadi bahasa pemrograman yang penting dalam menghadirkan pengalaman pengguna yang interaktif, fungsionalitas yang kaya, dan aplikasi web yang inovatif.
+  </p>
+</div>
   </main>
       `;
   },
